8dc419e6876b418f92d6667b679faa6cb866f22f
Adam Peacock <apeacock@calldatasystems.com>
2025-12-29 13:42:42 -0600

    Trigger stage deployment v2

--- terraform/environments/stage/main.tf
+++ terraform/environments/stage/main.tf
@@ -1,6 +1,6 @@
 # CallData Foundation Platform - Stage Environment
 # All-in-one deployment using cd-stg VPC
-# Trigger deployment: 2024-12-29
+# Trigger deployment: 2024-12-29 v2
 
 locals {
   project_name = "calldata-foundation"
